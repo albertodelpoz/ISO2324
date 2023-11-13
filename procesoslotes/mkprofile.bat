97ac38fb60526292f88946cddc1b0b196a1fd1f8
albertodelpoz <150673626+albertodelpoz@users.noreply.github.com>
2023-11-13 12:27:19 +0100

    Add files via upload

---
+++ procesoslotes/mkprofile.bat
@@ -0,0 +1,24 @@
+@echo off
+REM AUTOR : ALBERTO DEL POZO MAGANEZ
+REM FECHA: 13:11:2023
+REM
+mkdir %USERPROFILES%\asir %USERPROFILES%\dam %USERPROFILES%\smr %USERPROFILES%\daw %USERPROFILES%\ceti
+mkdir %USERPROFILES%\asir\docs  %USERPROFILES%\asir\pract %USERPROFILES%\asir\exam
+mkdir %USERPROFILES%\dam\docs  %USERPROFILES%\dam\pract %USERPROFILES%\dam\exam
+mkdir %USERPROFILES%\smr\docs  %USERPROFILES%\smr\pract %USERPROFILES%\smr\exam
+mkdir %USERPROFILES%\daw\docs  %USERPROFILES%\daw\pract %USERPROFILES%\daw\exam
+mkdir %USERPROFILES%\ceti\docs  %USERPROFILES%\ceti\pract %USERPROFILES%\ceti\exam
+	
+ 
+ 
+ 
+ 
+ 
+ -
+ 
+ 
+ 
+ 
+ 
+ 
+ 
